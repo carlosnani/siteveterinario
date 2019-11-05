d9cca2461f504e0074090c6308dbdb16dd04d21f
carlosnani <carlosnani80@gmail.com>
2019-11-05 06:15:23 -0300

    inicio

---
+++ gulpfile.js
@@ -0,0 +1,86 @@
+// Adiciona os modulos instalados
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
+const browserSync = require('browser-sync').create();
+const concat = require('gulp-concat');
+const babel = require('gulp-babel');
+const uglify = require('gulp-uglify');
+
+// Funçao para compilar o SASS e adicionar os prefixos
+function compilaSass() {
+  return gulp
+  .src('css/scss/*.scss')
+  .pipe(sass({
+    outputStyle: 'compressed',
+    //outputStyle: 'expanded'
+  }))
+  .pipe(autoprefixer({
+    browsers: ['last 2 versions'],
+    cascade: false
+  }))
+  .pipe(gulp.dest('css/'))
+  .pipe(browserSync.stream());
+}
+
+// Tarefa de gulp para a função de SASS
+gulp.task('sass', function(done){
+  compilaSass();
+  done();
+});
+
+// Função para juntar o JS
+function gulpJS() {
+  return gulp
+  .src('js/javascript/*.js')
+  .pipe(concat('app.js'))
+  .pipe(babel({
+    presets: ['env']
+  }))
+  .pipe(uglify())
+  .pipe(gulp.dest('js/'))
+  .pipe(browserSync.stream())
+}
+
+gulp.task('mainjs', gulpJS);
+
+// JS Plugins
+// function pluginJS() {
+//   return gulp
+//   .src([
+//     'node_modules/jquery/dist/jquery.min.js',
+//     'node_modules/moment/min/moment.min.js',
+//     'js/plugins/*.js'
+//   ])
+//   .pipe(concat('plugins.js'))
+//   .pipe(gulp.dest('js/'))
+//   .pipe(browserSync.stream())
+// }
+
+// gulp.task('pluginjs', pluginJS);
+
+// Função para iniciar o browser
+function browser() {
+  browserSync.init({
+    server: {
+      baseDir: "./"
+    }
+  });
+}
+
+// Tarefa para iniciar o browser-sync
+gulp.task('browser-sync', browser);
+
+// Função de watch do Gulp
+function watch() {
+  gulp.watch('css/scss/*.scss', compilaSass);
+  gulp.watch('js/javascript/*.js', gulpJS);
+  // gulp.watch('js/plugins/*.js', pluginJS);
+  gulp.watch(['*.html']).on('change', browserSync.reload);
+}
+
+// Inicia a tarefa de watch
+gulp.task('watch', watch);
+
+// Tarefa padrão do Gulp, que inicia o watch e o browser-sync
+gulp.task('default', gulp.parallel('watch', 'browser-sync', 'sass', 'mainjs'));
